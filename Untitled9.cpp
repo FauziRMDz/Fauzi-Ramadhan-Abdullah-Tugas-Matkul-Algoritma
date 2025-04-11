151cd8f3f7790cabce9364ec16ee090cf65e1c9c
FauziRMDz <fauzirmda@gmail.com>
2025-04-11 16:12:11 +0700

    Add files via upload

---
+++ Untitled9.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string> 
+using namespace std; main()
+{ string nama, kelas, nim, hobi, umur;
+
+cout<<"Masukan Nim"<<endl;
+cin>>nim;
+cout<<"Masukan Nama"<<endl;
+cin>>nama;
+cout<<"Masukan Kelas"<<endl;
+cin>>kelas;
+cout<<"Masukan Hobi"<<endl;
+cin>>hobi;
+cout<<"Masukan Umur"<<endl;
+cin>>umur;
+
+
+cout<<"==data diri=="<<endl;
+cout<<"nim:"<<nim<<endl;
+cout<<"nama :"<<nama<<endl;
+cout<<"kelas:"<<kelas<<endl;
+cout<<"hobi:"<<hobi<<endl;
+cout<<"umur:"<<umur<<endl;
+
+
+}
